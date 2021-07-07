3408349c5983a920761beeb7fc8a5aa8cac480cc
Cristian <cristiancaraballo112@gmail.com>
2021-07-07 17:07:15 -0400

    script for check or sow api keys

---
+++ scripts/seedApiKeys.js
@@ -0,0 +1,41 @@
+const MongoLib = require('../lib/mongo');
+const { adminApiKeyToken, publicApiKeyToken } = require('../config').config;
+
+const adminScopes = ['signin:auth', 'signup:auth', 'create:movies', 'read:movies', 'update:movies', 'delete:movies'];
+
+const publicScopes = ['signin:auth', 'signup:auth', 'read:movies'];
+
+const apiKeys = [
+  {
+    token: adminApiKeyToken,
+    scopes: adminScopes,
+  },
+  {
+    token: publicApiKeyToken,
+    scopes: publicScopes,
+  },
+];
+
+async function seedApiKeys() {
+  try {
+    const mongoDB = new MongoLib();
+
+    console.log('Cheking ApiKeys...');
+    const promises = apiKeys.map(async (apiKey) => {
+      const [existApiKey] = await mongoDB.getAll('api-keys', { token: apiKey.token });
+
+      if (!existApiKey) {
+        await mongoDB.create('api-keys', apiKey);
+        console.log('ApiKey Created');
+      }
+    });
+
+    await Promise.all(promises);
+    process.exit(0);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+}
+
+seedApiKeys();
